3d375c2b2f123c268a39f8d038add974a7678ad0
Wu Jin <wujin.developer@gmail.com>
2023-10-01 22:51:35 +0800

    update

--- src/media/video-encode/platform/win/platform-video-encode.c
+++ src/media/video-encode/platform/win/platform-video-encode.c
@@ -5,6 +5,8 @@
 #define SAFE_RELEASE(u) \
     do { if ((u) != NULL) (u)->lpVtbl->Release(u); (u) = NULL; } while(0)
 
+once_flag once = ONCE_FLAG_INIT;
+
 static bool _d3d11_create_device(ID3D11Device** device, ID3D11DeviceContext** context) {
 	HRESULT          hr;
 	IDXGIFactory5* p_dxgi_factory5; /* using five version to control tearing */
@@ -55,7 +57,51 @@ static bool _d3d11_create_device(ID3D11Device** device, ID3D11DeviceContext** co
 	return true;
 }
 
-video_encode_frame_t* platform_video_encode(video_capture_frame_t* frame) {
+static void _d3d11_create_video_device(ID3D11Device* device, ID3D11DeviceContext* context, ID3D11VideoDevice** video_device, ID3D11VideoContext** video_context) {
+	HRESULT hr;
+	if (FAILED(hr = ID3D11Device_QueryInterface(device, &IID_ID3D11VideoDevice, video_device)))
+	{
+		printf("query video device failed: 0x%x.\n", hr);
+		return false;
+	}
+	if (FAILED(hr = ID3D11DeviceContext_QueryInterface(context, &IID_ID3D11VideoContext, video_context)))
+	{
+		printf("query video device context failed: 0x%x.\n", hr);
+		return false;
+	}
+}
 
+static void _bgra_to_nv12(ID3D11Texture2D* bgra, ID3D11Texture2D* nv12){
+
+}
+
+video_encode_frame_t* platform_video_encode(video_capture_frame_t* frame) {
+	ID3D11Device* device;
+	ID3D11DeviceContext* context;
+	ID3D11VideoDevice* video_device;
+	ID3D11VideoContext* video_context;
+
+	if (!atomic_flag_test_and_set(once)) {
+		_d3d11_create_device(&device, &context);
+		_d3d11_create_video_device(device, context, &video_device, &video_context);
+	}
+	ID3D11Texture2D* texture = NULL;
+	D3D11_TEXTURE2D_DESC desc;
+	memset(&desc, 0, sizeof(D3D11_TEXTURE2D_DESC));
+	desc.Width = frame->width;
+	desc.Height = frame->height;
+	desc.MipLevels = 1;
+	desc.ArraySize = 1;
+	desc.Format = DXGI_FORMAT_NV12;
+	desc.SampleDesc.Count = 1;
+	desc.Usage = D3D11_USAGE_DEFAULT;
+	desc.BindFlags = D3D11_BIND_RENDER_TARGET;
+	desc.CPUAccessFlags = 0;
+
+	if (FAILED(ID3D11Device_CreateTexture2D(device, &desc, NULL, &texture))) {
+		printf("faliled to create d3d11texture\n");
+		return NULL;
+	}
+	_bgra_to_nv12(frame->frame, texture);
 	return NULL;
 }
--- src/orbiter-server.c
+++ src/orbiter-server.c
@@ -1,5 +1,3 @@
-
-
 #include "cdk.h"
 #include "common/synchronized-queue.h"
 #include "media/video-capture/video-capture.h"
@@ -13,13 +11,17 @@ int video_capture_thread(void* param) {
 
 int video_encode_thread(void* param) {
 	synchronized_queue_t** queues = param;
+	synchronized_queue_t* in = queues[0];
+	synchronized_queue_t* out = queues[1];
+
+	free(queues);
 
 	while (true) {
 		video_capture_frame_t* frame
-			= synchronized_queue_data(synchronized_queue_dequeue(queues[0]), video_capture_frame_t, node);
+			= synchronized_queue_data(synchronized_queue_dequeue(in), video_capture_frame_t, node);
 
 		video_encode_frame_t* bitstream = video_encode(frame);
-		synchronized_queue_enqueue(queues[1], &bitstream->node);
+		synchronized_queue_enqueue(out, &bitstream->node);
 	}
 	return 0;
 }
@@ -31,7 +33,13 @@ void create_video_capture_thread(synchronized_queue_t* queue) {
 }
 
 void create_video_encode_thread(synchronized_queue_t* in, synchronized_queue_t* out) {
-	synchronized_queue_t* queues[] = {in, out};
+	synchronized_queue_t** queues = malloc(sizeof(void*) * 2);
+	if (!queues) {
+		return;
+	}
+	queues[0] = in;
+	queues[1] = out;
+
 	thrd_t tid;
 	thrd_create(&tid, video_encode_thread, queues);
 	thrd_detach(tid);
@@ -49,7 +57,9 @@ int main(void) {
 
 	
 
-	
+	while (true) {
+		cdk_time_sleep(5000);
+	}
 
 	synchronized_queue_destroy(&captured);
 	synchronized_queue_destroy(&encoded);
